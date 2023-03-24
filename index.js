9dc42cf47a5bb7f5ac9f235f81e71f54ed7e3336
Ngozi1 <103275987+Ngozi1@users.noreply.github.com>
2023-03-24 05:38:45 -0700

    my commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load' , () => {
-    const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#new-task-input");
+    const form = document.querySelector("#task-form");
+    const input = document.querySelector("#task-input");
     const list_el = document.querySelector("#tasks");
    
 
@@ -8,7 +8,7 @@ window.addEventListener('load' , () => {
         e.preventDefault();
     
 
-       const task = input.Value;
+       const task = input.value;
 
        if (!task) {
           alert("please fill out the task");
@@ -21,7 +21,7 @@ window.addEventListener('load' , () => {
         
       const task_content_el = document.createElement("div");
       task_content_el.classList.add("content");
-      task_content_el.innerText = task;
+    //   task_content_el.innerText = task;
 
       task_el.appendChild(task_content_el);
 
